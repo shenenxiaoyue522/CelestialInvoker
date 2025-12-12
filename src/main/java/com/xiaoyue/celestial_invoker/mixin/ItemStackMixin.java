ce580455e3e642d84a792fdf3236fbf21d8a4ae5
shenenxiaoyue <3371025687@qq.com>
2025-12-13 00:56:39 +0800

    aabb

---
+++ src/main/java/com/xiaoyue/celestial_invoker/mixin/ItemStackMixin.java
@@ -0,0 +1,8 @@
+package com.xiaoyue.celestial_invoker.mixin;
+
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+}
