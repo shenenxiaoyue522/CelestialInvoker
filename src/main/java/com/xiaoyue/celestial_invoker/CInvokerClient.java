a8810813a0fdb9dad0ee76639bf1c376246040cb
shenenxiaoyue <3371025687@qq.com>
2026-02-06 00:19:01 +0800

    aabb

--- src/main/java/com/xiaoyue/celestial_invoker/CInvokerClient.java
+++ src/main/java/com/xiaoyue/celestial_invoker/CInvokerClient.java
@@ -1,14 +1,12 @@
 package com.xiaoyue.celestial_invoker;
 
-import com.xiaoyue.celestial_invoker.content.ancillary.helper.ItemBarHelper;
 import com.xiaoyue.celestial_invoker.content.client.HudOverlayHandler;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.event.RegisterItemDecorationsEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import static com.xiaoyue.celestial_invoker.CelestialInvoker.MODID;
 
@@ -21,12 +19,6 @@ public class CInvokerClient {
 
     @SubscribeEvent
     public static void initHud(RegisterGuiOverlaysEvent event) {
-        event.registerBelowAll("custom_bar_handler", new HudOverlayHandler());
+        event.registerAbove(VanillaGuiOverlay.AIR_LEVEL.id(), "hud_manager", new HudOverlayHandler());
     }
-
-    @SubscribeEvent
-    public static void initItemDecor(RegisterItemDecorationsEvent event) {
-        ForgeRegistries.ITEMS.getValues().forEach(item -> event.register(item, new ItemBarHelper()));
-    }
-
 }
