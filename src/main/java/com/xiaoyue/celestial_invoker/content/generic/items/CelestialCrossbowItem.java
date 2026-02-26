2586ef72a256f827c5157f108418535cd4645cc5
shenenxiaoyue <3371025687@qq.com>
2026-02-26 22:23:44 +0800

    aabb

---
+++ src/main/java/com/xiaoyue/celestial_invoker/content/generic/items/CelestialCrossbowItem.java
@@ -0,0 +1,125 @@
+package com.xiaoyue.celestial_invoker.content.generic.items;
+
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.ChargedProjectiles;
+import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CelestialCrossbowItem extends CrossbowItem {
+
+    private boolean startSoundPlayed = false;
+    private boolean midLoadSoundPlayed = false;
+
+    public CelestialCrossbowItem(Properties properties) {
+        super(properties);
+    }
+
+    public int getPullTime(ItemStack stack, LivingEntity shooter) {
+        return 25;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+        ChargedProjectiles projectiles = stack.get(DataComponents.CHARGED_PROJECTILES);
+        if (projectiles != null && !projectiles.isEmpty()) {
+            this.performShooting(level, player, hand, stack, getShootingPower(projectiles), 1.0F, null);
+            return InteractionResultHolder.consume(stack);
+        } else if (!player.getProjectile(stack).isEmpty()) {
+            this.startSoundPlayed = false;
+            this.midLoadSoundPlayed = false;
+            player.startUsingItem(hand);
+            return InteractionResultHolder.consume(stack);
+        } else {
+            return InteractionResultHolder.fail(stack);
+        }
+    }
+
+    public float getShootingPower(ChargedProjectiles projectile) {
+        return projectile.contains(Items.FIREWORK_ROCKET) ? 1.6f : 3.15f;
+    }
+
+    @Override
+    public void onUseTick(Level level, LivingEntity livingEntity, ItemStack stack, int count) {
+        if (!level.isClientSide) {
+            ChargingSounds crossbowitem$chargingsounds = this.getChargingSounds(stack);
+            float f = (float)(stack.getUseDuration(livingEntity) - count) / (float)getChargeDuration(stack, livingEntity);
+            if (f < 0.2F) {
+                this.startSoundPlayed = false;
+                this.midLoadSoundPlayed = false;
+            }
+            if (f >= 0.2F && !this.startSoundPlayed) {
+                this.startSoundPlayed = true;
+                crossbowitem$chargingsounds.start().ifPresent((p_352849_) -> level.playSound((Player)null, livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(), (SoundEvent)p_352849_.value(), SoundSource.PLAYERS, 0.5F, 1.0F));
+            }
+            if (f >= 0.5F && !this.midLoadSoundPlayed) {
+                this.midLoadSoundPlayed = true;
+                crossbowitem$chargingsounds.mid().ifPresent((p_352855_) -> level.playSound((Player)null, livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(), (SoundEvent)p_352855_.value(), SoundSource.PLAYERS, 0.5F, 1.0F));
+            }
+        }
+    }
+
+    ChargingSounds getChargingSounds(ItemStack stack) {
+        ChargingSounds sounds = new ChargingSounds(Optional.of(SoundEvents.CROSSBOW_LOADING_START), Optional.of(SoundEvents.CROSSBOW_LOADING_MIDDLE), Optional.of(SoundEvents.CROSSBOW_LOADING_END));
+        return EnchantmentHelper.pickHighestLevel(stack, EnchantmentEffectComponents.CROSSBOW_CHARGING_SOUNDS).orElse(sounds);
+    }
+
+    @Override
+    public int getUseDuration(ItemStack stack, LivingEntity entity) {
+        return getChargeDuration(stack, entity) + 3;
+    }
+
+    public static int getChargeDuration(ItemStack stack, LivingEntity shooter) {
+        float based = 1.25f;
+        if (stack.getItem() instanceof CelestialCrossbowItem item) {
+            based = item.getPullTime(stack, shooter) / 20f;
+        }
+        based = EnchantmentHelper.modifyCrossbowChargingTime(stack, shooter, based);
+        return Mth.floor(based * 20f);
+    }
+
+    @Override
+    public void releaseUsing(ItemStack stack, Level level, LivingEntity entityLiving, int timeLeft) {
+        int i = this.getUseDuration(stack, entityLiving) - timeLeft;
+        float f = getPowerForTime(i, stack, entityLiving);
+        if (f >= 1.0F && !isCharged(stack) && tryLoadProjectiles(entityLiving, stack)) {
+            ChargingSounds crossbowitem$chargingsounds = this.getChargingSounds(stack);
+            crossbowitem$chargingsounds.end().ifPresent((s) -> level.playSound(null, entityLiving.getX(),
+                    entityLiving.getY(), entityLiving.getZ(), s.value(), entityLiving.getSoundSource(),
+                    1.0F, 1.0F / (level.getRandom().nextFloat() * 0.5F + 1.0F) + 0.2F));
+        }
+    }
+
+    private static boolean tryLoadProjectiles(LivingEntity shooter, ItemStack crossbow) {
+        List<ItemStack> list = draw(crossbow, shooter.getProjectile(crossbow), shooter);
+        if (!list.isEmpty()) {
+            crossbow.set(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.of(list));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static float getPowerForTime(int timeLeft, ItemStack stack, LivingEntity shooter) {
+        float charged = (float) timeLeft / (float) getChargeDuration(stack, shooter);
+        if (charged > 1.0F) {
+            charged = 1.0F;
+        }
+        return charged;
+    }
+}
