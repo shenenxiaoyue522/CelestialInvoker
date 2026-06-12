40c099eccbff8bd8a7628c2b816622b239a7b3b8
shenenxiaoyue <3371025687@qq.com>
2026-06-12 20:23:46 +0800

    aabb

--- src/main/java/com/xiaoyue/celestial_invoker/content/generic/shared/NetworkHandler.java
+++ src/main/java/com/xiaoyue/celestial_invoker/content/generic/shared/NetworkHandler.java
@@ -1,8 +1,14 @@
 package com.xiaoyue.celestial_invoker.content.generic.shared;
 
 import com.xiaoyue.celestial_invoker.CelestialInvoker;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class NetworkHandler {
 
@@ -13,6 +19,10 @@ public class NetworkHandler {
             () -> VERSION, VERSION::equals, VERSION::equals);
 
     public static void register() {
-        INSTANCE.registerMessage(ID++, ClickEmptyPayload.class, ClickEmptyPayload::encode, ClickEmptyPayload::decode, ClickEmptyPayload::handle);
+        registerMSG(ClickEmptyPayload.class, ClickEmptyPayload::encode, ClickEmptyPayload::decode, ClickEmptyPayload::handle);
+    }
+
+    public static <MSG> void registerMSG(Class<MSG> msg, BiConsumer<MSG, FriendlyByteBuf> encoder, Function<FriendlyByteBuf, MSG> decoder, BiConsumer<MSG, Supplier<NetworkEvent.Context>> ctx) {
+        INSTANCE.registerMessage(ID++, msg, encoder, decoder, ctx);
     }
 }
